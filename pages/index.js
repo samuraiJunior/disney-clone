bf1b3c553027ca03d77cb15522bdfa2d4c037e7b
samuraiJunior <galabuevoleg@mail.ru>
2022-07-09 12:23:56 +0300

    refactor

--- pages/index.js
+++ pages/index.js
@@ -18,8 +18,6 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-
-      <Header />
       <Slider />
       <div className={styles.container}>
         <Brands />
